0be6ee68874ef61dc9cb275f9675de5ba3d1c1b5
Marvin9 <marvinduff97@gmail.com>
2020-08-15 20:37:12 +0530

    feat: Header

---
+++ src/pages/Dashboard/Header.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Transition } from 'react-transition-group';
+import { Button } from 'baseui/button';
+import { H6 } from 'baseui/typography';
+import { HeaderNavigation, ALIGN, StyledNavigationList } from 'baseui/header-navigation';
+
+import rootReducer from '../../store/reducers';
+
+interface HeaderType {
+  email: string | undefined;
+}
+
+const Header: React.FC<HeaderType> = ({ email }) => (
+  <HeaderNavigation
+    overrides={{
+      Root: {
+        style: {
+          height: '80px',
+        },
+      },
+    }}
+  >
+    <StyledNavigationList $align={ALIGN.center}>
+      <H6
+        overrides={{
+          Block: {
+            style: {
+              lineHeight: 0,
+              marginTop: 'auto',
+              marginBottom: 'auto',
+            },
+          },
+        }}
+      >
+        {email}
+      </H6>
+      <Button
+        overrides={{
+          BaseButton: {
+            style: {
+              height: '50px',
+              marginTop: 'auto',
+              marginBottom: 'auto',
+              marginLeft: '50px',
+            },
+          },
+        }}
+      >
+        Log out
+      </Button>
+    </StyledNavigationList>
+  </HeaderNavigation>
+);
+
+const mapStateToProps = (state: ReturnType<typeof rootReducer>) => ({
+  email: state.AuthReducer.userData?.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
-import * as logger from '../../utils/logger';
-import { APIList } from '../../global';
+// import * as logger from '../../utils/logger';
+// import { APIList } from '../../global';
+
+import Header from './Header';
 
 const Dashboard: React.FC = () => {
-  React.useEffect(() => {
-    logger.plainLog(`Fetching ${APIList.GET_REPORTS}`);
-    axios
-      .get(APIList.GET_REPORTS, { withCredentials: true })
-      .then((response) => {
-        logger.plainLog('Response: ');
-        logger.plainLog(`${response}`);
-      })
-      .catch((e) => {
-        logger.errorLog(Dashboard, e);
-      });
-  }, []);
-  return <>Hello</>;
+  return <Header />;
 };
 
 export default Dashboard;
