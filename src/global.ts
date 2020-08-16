7343fbaa6f107a95454f8060d0d22c150d9a9e36
Marvin9 <marvinduff97@gmail.com>
2020-08-16 21:56:11 +0530

    feat: Logout func

--- src/global.ts
+++ src/global.ts
@@ -3,6 +3,7 @@ export const APIBase = 'http://localhost:8000';
 export const APIList = {
   LOGIN: `${APIBase}/auth/login`,
   REGISTER: `${APIBase}/auth/register`,
+  LOGOUT: `${APIBase}/auth/logout`,
   GET_REPORT: `${APIBase}/api/report`,
   GET_INSTANCES: `${APIBase}/api/instances`,
   ADD_INSTANCE: `${APIBase}/api/instance`,
--- src/pages/Dashboard/Header.tsx
+++ src/pages/Dashboard/Header.tsx
@@ -1,59 +1,89 @@
-import React from 'react';
+import React, { Dispatch, useState } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import { Button } from 'baseui/button';
 import { H6 } from 'baseui/typography';
 import { HeaderNavigation, ALIGN, StyledNavigationList } from 'baseui/header-navigation';
 
+import { APIList } from '../../global';
+import { routes } from '../../routes';
+import { errorLog } from '../../utils';
+
 import rootReducer from '../../store/reducers';
+import { redirectTo_ } from '../../store/actions';
+import { REDIRECT_TO } from '../../store/actionTypes';
 
 interface HeaderType {
   email: string | undefined;
+  logoutRedirect(): void;
 }
 
-const Header: React.FC<HeaderType> = ({ email }) => (
-  <HeaderNavigation
-    overrides={{
-      Root: {
-        style: {
-          height: '80px',
+const Header: React.FC<HeaderType> = ({ email, logoutRedirect }) => {
+  const [logoutLoading, updateLogoutLoading] = useState(false);
+
+  const logout = async () => {
+    updateLogoutLoading(true);
+    try {
+      await axios.post(APIList.LOGOUT, null, { withCredentials: true });
+      logoutRedirect();
+    } catch (e) {
+      errorLog(Header, e);
+    } finally {
+      updateLogoutLoading(false);
+    }
+  };
+
+  return (
+    <HeaderNavigation
+      overrides={{
+        Root: {
+          style: {
+            height: '80px',
+          },
         },
-      },
-    }}
-  >
-    <StyledNavigationList $align={ALIGN.center}>
-      <H6
-        overrides={{
-          Block: {
-            style: {
-              lineHeight: 0,
-              marginTop: 'auto',
-              marginBottom: 'auto',
+      }}
+    >
+      <StyledNavigationList $align={ALIGN.center}>
+        <H6
+          overrides={{
+            Block: {
+              style: {
+                lineHeight: 0,
+                marginTop: 'auto',
+                marginBottom: 'auto',
+              },
             },
-          },
-        }}
-      >
-        {email}
-      </H6>
-      <Button
-        overrides={{
-          BaseButton: {
-            style: {
-              height: '50px',
-              marginTop: 'auto',
-              marginBottom: 'auto',
-              marginLeft: '50px',
+          }}
+        >
+          {email}
+        </H6>
+        <Button
+          overrides={{
+            BaseButton: {
+              style: {
+                height: '50px',
+                marginTop: 'auto',
+                marginBottom: 'auto',
+                marginLeft: '50px',
+              },
             },
-          },
-        }}
-      >
-        Log out
-      </Button>
-    </StyledNavigationList>
-  </HeaderNavigation>
-);
+          }}
+          isLoading={logoutLoading}
+          onClick={logout}
+        >
+          Log out
+        </Button>
+      </StyledNavigationList>
+    </HeaderNavigation>
+  );
+};
 
 const mapStateToProps = (state: ReturnType<typeof rootReducer>) => ({
   email: state.AuthReducer.userData ? state.AuthReducer.userData.email : undefined,
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = (dispatch: Dispatch<REDIRECT_TO>) => ({
+  logoutRedirect: () => dispatch(redirectTo_(routes.AUTH_PAGE)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
